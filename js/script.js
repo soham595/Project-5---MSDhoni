31e15d7681cd7d9d3a68a92bfbf755c7b8e3dde0
Soham Das <soham5959595@gmail.com>
2016-10-19 22:52:53 +0530

    created
    complete mockup 1st commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+  $('.download').click(function() {
+    window.location.href = "https://www.google.co.in/search?safe=off&site=&source=hp&q=download+ms+dhoni+720p&oq=download+ms+dhoni+720p&gs_l=hp.3...5161.11070.0.11323.24.17.0.3.3.0.417.3056.2-6j2j2.10.0....0...1c.1.64.hp..12.8.1570.0..0j35i39k1j0i67k1j0i10i67k1j0i131k1.4LH6liWvKQA";
+  })
+
+  $('.back').click(function() {
+    window.history.back();
+  })
+})
